848d25442e7bdaa5e2b25b19696e46f01882b864
Darwin757 <darwin.rocker@gmail.com>
2023-11-12 22:11:47 +0100

    Add Response Objects

---
+++ src/main/java/com/darwin/ecommerceapi/controller/response/InventoryResponse.java
@@ -0,0 +1,33 @@
+package com.darwin.ecommerceapi.controller.response;
+
+public class InventoryResponse {
+    private Long id;
+    private Long productId;
+    private int quantityAvailable;
+
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public int getQuantityAvailable() {
+        return quantityAvailable;
+    }
+
+    public void setQuantityAvailable(int quantityAvailable) {
+        this.quantityAvailable = quantityAvailable;
+    }
+}
