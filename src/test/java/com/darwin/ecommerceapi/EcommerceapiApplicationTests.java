55b68554658d01f0f2cdfa1617fffff3e34384a4
Darwin757 <darwin.rocker@gmail.com>
2023-11-12 20:03:46 +0100

    initial commit

---
+++ src/test/java/com/darwin/ecommerceapi/EcommerceapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darwin.ecommerceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
