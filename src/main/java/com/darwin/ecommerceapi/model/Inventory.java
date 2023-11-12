d1403fc3098bc5d381102bbe74f703eb66d7e5f2
Darwin757 <darwin.rocker@gmail.com>
2023-11-12 21:18:16 +0100

    Add Inventory POJO

---
+++ src/main/java/com/darwin/ecommerceapi/model/Inventory.java
@@ -0,0 +1,59 @@
+package com.darwin.ecommerceapi.model;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "inventory")
+public class Inventory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column(name = "quantity_available")
+    private int quantityAvailable;
+
+    @Column(name = "last_updated")
+    private java.sql.Timestamp lastUpdated;
+
+    // Getters and Setters
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getQuantityAvailable() {
+        return quantityAvailable;
+    }
+
+    public void setQuantityAvailable(int quantityAvailable) {
+        this.quantityAvailable = quantityAvailable;
+    }
+
+    public Timestamp getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(Timestamp lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+}
+
