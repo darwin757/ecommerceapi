7c350251025ef05d20b1483b6d02e43a48ecefbd
Darwin757 <darwin.rocker@gmail.com>
2023-11-12 22:58:01 +0100

    Fix dtoToResponse Method

--- src/main/java/com/darwin/ecommerceapi/mapper/CustomerMapper.java
+++ src/main/java/com/darwin/ecommerceapi/mapper/CustomerMapper.java
@@ -1,6 +1,7 @@
 package com.darwin.ecommerceapi.mapper;
 
 import com.darwin.ecommerceapi.controller.request.CustomerRequest;
+import com.darwin.ecommerceapi.controller.response.CustomerResponse;
 import com.darwin.ecommerceapi.dto.CustomerDTO;
 import com.darwin.ecommerceapi.model.Customer;
 import org.mapstruct.Mapper;
@@ -9,6 +10,6 @@ import org.mapstruct.Mapper;
 public interface CustomerMapper {
     CustomerDTO customerToCustomerDTO(Customer customer);
     Customer customerDTOToCustomer(CustomerDTO customerDTO);
-    CustomerDTO dtoToResponse(CustomerDTO customerDTO);
+    CustomerResponse dtoToResponse(CustomerDTO customerDTO);
     CustomerDTO requestToDto(CustomerRequest customerRequest);
 }
