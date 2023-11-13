4f003ab06a7e6780836d94cff96f593776c4e6d8
Darwin757 <darwin.rocker@gmail.com>
2023-11-13 18:57:13 +0100

    add MyUserDetailsService

---
+++ src/main/java/com/darwin/ecommerceapi/security/MyUserDetailsService.java
@@ -0,0 +1,36 @@
+package com.darwin.ecommerceapi.security;
+
+import com.darwin.ecommerceapi.model.User;  // Your User entity
+import com.darwin.ecommerceapi.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                getAuthorities(user));
+    }
+
+    private Collection<? extends GrantedAuthority> getAuthorities(User user) {
+        return user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toList());
+    }
+}
