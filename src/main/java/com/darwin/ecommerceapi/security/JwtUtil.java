8524c8a0c173ba7d2b2d546a90217f1edab3ed86
Darwin757 <darwin.rocker@gmail.com>
2023-11-13 18:39:04 +0100

    implement more methods in JwtUtil

--- src/main/java/com/darwin/ecommerceapi/security/JwtUtil.java
+++ src/main/java/com/darwin/ecommerceapi/security/JwtUtil.java
@@ -1,22 +1,50 @@
 package com.darwin.ecommerceapi.security;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import java.util.Date;
+import java.util.function.Function;
 
 public class JwtUtil {
 
-    private String secretKey = "yourSecretKey"; // You should store this securely
+    private String secretKey = "yourSecretKey";
 
-    public String generateToken(String username) {
+    public String generateToken(UserDetails userDetails) {
         return Jwts.builder()
-                .setSubject(username)
+                .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10 hours
                 .signWith(SignatureAlgorithm.HS256, secretKey)
                 .compact();
     }
 
-    // Methods to validate and parse JWT (to be implemented)
+    public Boolean validateToken(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
+    }
 }
 
