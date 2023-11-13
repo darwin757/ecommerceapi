f3e46c437ccdbb0f0c5a0a1bb4bc59baba54da29
Darwin757 <darwin.rocker@gmail.com>
2023-11-13 18:39:46 +0100

    Add User and Role Entities

---
+++ src/main/java/com/darwin/ecommerceapi/model/Role.java
@@ -0,0 +1,34 @@
+package com.darwin.ecommerceapi.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;  // e.g., ROLE_ADMIN, ROLE_USER
+
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
