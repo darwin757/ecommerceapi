9692b01d84111a9ef315535f4894882505498e0a
Darwin757 <darwin.rocker@gmail.com>
2023-11-13 18:47:50 +0100

    Add Request and Response Objects for Authentication

---
+++ src/main/java/com/darwin/ecommerceapi/controller/response/AuthenticationResponse.java
@@ -0,0 +1,15 @@
+package com.darwin.ecommerceapi.controller.response;
+
+
+public class AuthenticationResponse {
+
+    private final String jwt;
+
+    public AuthenticationResponse(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
+}
