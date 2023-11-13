70d591015a96351730154f7087e2fc12d506c94d
Darwin757 <darwin.rocker@gmail.com>
2023-11-13 18:21:39 +0100

    Add SecurityConfig

---
+++ src/main/java/com/darwin/ecommerceapi/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.darwin.ecommerceapi.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/public/**").permitAll() // public endpoints
+                .anyRequest().authenticated() // all other requests need authentication
+                .and()
+        // Add filters for JWT (to be implemented)
+        ;
+    }
+}
+
